9786a1cd6a19a13d7b447ceff08ef8acd0f02383
percygr <percyg@gmail.com>
2023-08-08 12:07:26 +0100

    add & get routes

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ const port = 3000;
 
 app.use(express.json());
 
+
 class Task {
     id: number;
     title: string;
@@ -23,12 +24,25 @@ class TaskList {
         this.tasks.push(task);
     }
 }
+const taskList = new TaskList();
+let nextTaskId = 1;
 
+// add a task
+app.post('/tasks', (req, res) => {
+    const task = new Task(nextTaskId, req.body.title);
+    taskList.addTask(task);
+    nextTaskId++; // Increment the next task ID for the next task
+    res.send(task);
+});
+
+// get all tasks
+app.get('/tasks', (req, res) => {
+    res.send(taskList.tasks);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-}
-);
+});
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
